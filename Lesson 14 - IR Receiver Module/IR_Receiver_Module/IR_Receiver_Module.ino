a0c36c752f34a55299aea1fbcd6d0f609dd94826
xyloz-quin <xylozquin@gmail.com>
2026-03-01 15:51:47 +0000

    Update IR_Receiver_Module.ino
    Updated to V4 code

--- Lesson 14 - IR Receiver Module/IR_Receiver_Module/IR_Receiver_Module.ino
+++ Lesson 14 - IR Receiver Module/IR_Receiver_Module/IR_Receiver_Module.ino
@@ -1,65 +1,64 @@
 //www.elegoo.com
 //2016.12.9
 
-#include "IRremote.h"
+//Update By Thomas Wright -  2026.03.01 - Notes Version 4 compatible
+#include <IRremote.hpp>
 
-/*-----( Global Constants )-----*/
-const int receiver = 11;      // Signal Pin of IR receiver to Arduino Digital Pin 11
+const int receiverPin = 11; // Connect IR receiver signal pin here
 
-/*-----( Declare objects )-----*/
-IRrecv irrecv(receiver);     // create instance of 'irrecv'
-decode_results results;      // create instance of 'decode_results'
-
-/*-----( Function )-----*/
-void translateIR() {          // takes action based on IR code received
-// describing Remote IR codes 
-
-  switch(results.value){
-    case 0xFFA25D: Serial.println("POWER"); break;
-    case 0xFFE21D: Serial.println("FUNC/STOP"); break;
-    case 0xFF629D: Serial.println("VOL+"); break;
-    case 0xFF22DD: Serial.println("FAST BACK");    break;
-    case 0xFF02FD: Serial.println("PAUSE");    break;
-    case 0xFFC23D: Serial.println("FAST FORWARD");   break;
-    case 0xFFE01F: Serial.println("DOWN");    break;
-    case 0xFFA857: Serial.println("VOL-");    break;
-    case 0xFF906F: Serial.println("UP");    break;
-    case 0xFF9867: Serial.println("EQ");    break;
-    case 0xFFB04F: Serial.println("ST/REPT");    break;
-    case 0xFF6897: Serial.println("0");    break;
-    case 0xFF30CF: Serial.println("1");    break;
-    case 0xFF18E7: Serial.println("2");    break;
-    case 0xFF7A85: Serial.println("3");    break;
-    case 0xFF10EF: Serial.println("4");    break;
-    case 0xFF38C7: Serial.println("5");    break;
-    case 0xFF5AA5: Serial.println("6");    break;
-    case 0xFF42BD: Serial.println("7");    break;
-    case 0xFF4AB5: Serial.println("8");    break;
-    case 0xFF52AD: Serial.println("9");    break;
-    case 0xFFFFFFFF: Serial.println(" REPEAT");break;  
-
-  default: 
-    Serial.print(" other button   ");
-    Serial.println(results.value);
-
-  }// End Case
-
-} //END translateIR
-
-void setup(){   /*----( SETUP: RUNS ONCE )----*/
+void setup() {
   Serial.begin(9600);
-  Serial.println("IR Receiver Button Decode"); 
-  irrecv.enableIRIn();           // Start the receiver
+  Serial.println("=== IR Remote Test ===");
 
-}/*--(end setup )---*/
+  // Initialize the IR sender and receiver
+  //IrSender.begin();  // no pin needed unless sending
+  IrReceiver.begin(receiverPin, ENABLE_LED_FEEDBACK);
+}
 
-void loop(){   /*----( LOOP: RUNS CONSTANTLY )----*/
-  if (irrecv.decode(&results))   // have we received an IR signal?
-  {
-    translateIR(); 
-    delay(500);                 // Do not get immediate repeat
-    irrecv.resume();            // receive the next value
-  }  
-}/* --(end main loop )-- */
+void loop() {
+  if (IrReceiver.decode()) {
+    uint32_t rawValue = IrReceiver.decodedIRData.command; // safer than decodedRawData
 
+    // Print raw value in HEX
+    Serial.print("Raw HEX: 0x");
+    Serial.println(rawValue, HEX);
 
+    // Print human-readable button name
+    printButtonName(rawValue);
+
+    IrReceiver.resume(); // ready for next signal
+  }
+}
+
+// Function to print button name for known NEC codes
+void printButtonName(uint32_t value) {
+  switch (value) {
+    case 0x45: Serial.println("Button: POWER"); break;
+    case 0x46: Serial.println("Button: FUNC/STOP"); break;
+    case 0x47: Serial.println("Button: VOL+"); break;
+    case 0x44: Serial.println("Button: FAST BACK"); break;
+    case 0x40: Serial.println("Button: PLAY/PAUSE"); break;
+    case 0x43: Serial.println("Button: FAST FORWARD"); break;
+    case 0x7: Serial.println("Button: DOWN"); break;
+    case 0x15: Serial.println("Button: VOL-"); break;
+    case 0x9: Serial.println("Button: UP"); break;
+    case 0x19: Serial.println("Button: EQ"); break;
+    case 0xD: Serial.println("Button: ST/REPT"); break;
+    case 0x16: Serial.println("Button: 0"); break;
+    case 0xC: Serial.println("Button: 1"); break;
+    case 0x18: Serial.println("Button: 2"); break;
+    case 0x5E: Serial.println("Button: 3"); break;
+    case 0x8: Serial.println("Button: 4"); break;
+    case 0x1C: Serial.println("Button: 5"); break;
+    case 0x5A: Serial.println("Button: 6"); break;
+    case 0x42: Serial.println("Button: 7"); break;
+    case 0x52: Serial.println("Button: 8"); break;
+    case 0x4A: Serial.println("Button: 9"); break;
+    //case 0x0: Serial.println("Button: REPEAT"); break; // not required kept in case as repeat now repeats the button pressed
+
+    default:
+      Serial.print("Button: Unknown (0x");
+      Serial.print(value, HEX);
+      Serial.println(")");
+  }
+}
